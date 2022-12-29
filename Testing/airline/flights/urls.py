8574307579d41d8ee26c26fa32640f183a321206
dristosun <107073369+dristosun@users.noreply.github.com>
2022-12-29 10:48:19 +0200

    Delete Testing/airline directory

--- Testing/airline/flights/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("addFlight", views.addFlight, name="addFlight"),
-    path("flight/<int:flightId>", views.flight, name="flight")
-]
